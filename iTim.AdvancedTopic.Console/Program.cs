f3ad286d0a941b998643d38defe1080be84e8f95
itimpg <itim@outlook.com>
2020-03-10 23:36:15 +0700

    Add project files.

---
+++ iTim.AdvancedTopic.Console/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace iTim.AdvancedTopic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteManuallyCancellableTaskAsync();
+
+            do
+            {
+            } while (true);
+        }
+         
+        private static Task<decimal> LongRunningCancellableOperation(int loop, CancellationToken cancellationToken)
+        {
+            Task<decimal> task = null;
+
+            // Start a task and return it
+            task = Task.Run(() =>
+            {
+                decimal result = 0;
+
+                // Loop for a defined number of iterations
+                for (int i = 0; i < loop; i++)
+                {
+                    // Check if a cancellation is requested, if yes,
+                    // throw a TaskCanceledException.
+
+                    Console.WriteLine(i);
+
+                    if (cancellationToken.IsCancellationRequested)
+                        throw new TaskCanceledException(task);
+
+                    // Do something that takes times like a Thread.Sleep in .NET Core 2.
+                    Thread.Sleep(100);
+                    result += i;
+                }
+
+                return result;
+            });
+
+            return task;
+        }
+
+        public static async Task ExecuteManuallyCancellableTaskAsync()
+        {
+            Console.WriteLine(nameof(ExecuteManuallyCancellableTaskAsync));
+
+            using (var cancellationTokenSource = new CancellationTokenSource())
+            {
+                // Creating a task to listen to keyboard key press
+                var keyBoardTask = Task.Run(() =>
+                {
+                    Console.WriteLine("Press enter to cancel");
+                    Console.ReadKey();
+
+                    // Cancel the task
+                    cancellationTokenSource.Cancel();
+                });
+
+                try
+                {
+                    var longRunningTask = LongRunningCancellableOperation(500, cancellationTokenSource.Token);
+
+                    var result = await longRunningTask;
+                    Console.WriteLine("Result {0}", result);
+                    Console.WriteLine("Press enter to continue");
+                }
+                catch (TaskCanceledException)
+                {
+                    Console.WriteLine("Task was cancelled");
+                }
+
+                await keyBoardTask;
+            }
+        }
+    }
+}
---
+++ iTim.AdvancedTopic/AdvancedTopics.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+
+namespace iTim.AdvancedTopic
+{
+    [TestFixture]
+    public class AsyncAwaitTester
+    {
+        public Task TaskA()
+        {
+            return new Task(() =>
+            {
+                Task.Delay(3);
+                Console.WriteLine("Task A");
+            });
+        }
+
+        public Task TaskB()
+        {
+            return new Task(() =>
+            {
+                Task.Delay(1);
+                Console.WriteLine("Task B");
+            });
+        }
+    }
+
+    public class AdvancedTopics
+    {
+        [Test]
+        public void Async_Await()
+        {
+            AsyncAwaitTester asyncAwaitTester = new AsyncAwaitTester();
+            asyncAwaitTester.TaskA();
+            asyncAwaitTester.TaskB();
+        }
+
+        [Test]
+        public void StackVSHeap()
+        {
+
+        }
+    }
+}
